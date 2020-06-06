1841df10c54ab0a193a4762409b8d633400e4393
João Emerson <joaoemersonufc@gmail.com>
2020-06-05 22:59:08 -0300

    [added] Mapa e formulários na página de Busca

--- Ecoleta/react/web/src/pages/CreatePoint/index.tsx
+++ Ecoleta/react/web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker } from "react-leaflet";
+import api from "../../services/api";
 
 import "./styles.css";
 
@@ -30,19 +32,79 @@ const CreatePoint = () => {
             <label htmlFor="name">Nome da entidade</label>
             <input type="text" name="name" id="name" />
           </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="whatsapp">WhatsApp</label>
+              <input type="text" name="whatsapp" id="whatsapp" />
+            </div>
+          </div>
         </fieldset>
 
         <fieldset>
           <legend>
             <h2>Endereço</h2>
+            <span>Selecione o endereço do mapa</span>
           </legend>
+
+          <Map center={[-5.1435472, -38.0948752]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-5.1435472, -38.0948752]} />
+          </Map>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+          </div>
         </fieldset>
 
         <fieldset>
           <legend>
             <h2>Itens de coleta</h2>
+            <span>Selecione um ou mais itens abaixo</span>
           </legend>
+
+          <ul className="items-grid">
+            <li className="selected">
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Oleo de cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Oleo de cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Oleo de cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Oleo de cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="teste" />
+              <span>Oleo de cozinha</span>
+            </li>
+          </ul>
         </fieldset>
+
+        <button type="submit">Cadastrar ponto de coleta</button>
       </form>
     </div>
   );
