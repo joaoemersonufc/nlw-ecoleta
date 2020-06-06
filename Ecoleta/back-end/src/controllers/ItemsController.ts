67b5a726c03122a47f4d477bd135c1a1a4bd92ac
João Emerson <joaoemersonufc@gmail.com>
2020-06-06 16:05:29 -0300

    [added] Finalizado sistema web com cadastro na api

--- Ecoleta/back-end/src/controllers/ItemsController.ts
+++ Ecoleta/back-end/src/controllers/ItemsController.ts
@@ -1,20 +1,21 @@
-import { Request, Response } from "express";
-import knex from "../database/connection";
+import { Request, Response } from 'express';
+import knex from '../database/connection';
 
 class ItemsController {
-  async index(req: Request, res: Response) {
-    const items = await knex("items").select("*");
+	async index(req: Request, res: Response) {
+		const items = await knex('items').select('*');
 
-    const serializedItems = items.map((item) => {
-      return {
-        title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
-      };
-    });
+		const serializedItems = items.map((item) => {
+			return {
+				id: item.id,
+				title: item.title,
+				image_url: `http://localhost:3333/uploads/${item.image}`,
+			};
+		});
 
-    return res.json(serializedItems);
-    //return res.json({ message: "Hello world" });
-  }
+		return res.json(serializedItems);
+		//return res.json({ message: "Hello world" });
+	}
 }
 
 export default ItemsController;
